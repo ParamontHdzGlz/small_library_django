1aaef004316f6d049718c9f550975c1c26e00038
Paramont <ricardo.paramont.hg@gmail.com>
2021-05-04 16:14:52 -0500

    update ALLOWED_HOSTS with site

--- local_library/settings.py
+++ local_library/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m(lu(-g&n_31*&
 DEBUG = True
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://blooming-fortress-18053.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
